e5a236a6128883b3ad74881ad704b8f458b62118
신유나 <ynshin@ibizsoftware.net>
2019-09-27 13:35:00 +0900

    709. To Lower Case

---
+++ src/ToLowerCase.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+
+/**
+ * @title	709. To Lower Case
+ *
+ * @desc	
+ * 			Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ * 
+ */
+
+public class ToLowerCase {
+	
+	public String toLowerCase(String str) {
+        String result = "";
+        
+        result = str.toLowerCase();
+        
+		return result;
+    }
+	
+ 	@Test
+    public void test() {
+		String str = "Hello";
+
+		String result = toLowerCase(str); // 함수 실행
+		
+        assertEquals("hello", result); // 결과 확인
+    }
+}
