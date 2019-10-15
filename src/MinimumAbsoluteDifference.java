2ac38bd017de03ce98dec58584c1c97d5042c86e
신유나 <ynshin@ibizsoftware.net>
2019-10-15 10:03:21 +0900

    solution 383.

--- src/MinimumAbsoluteDifference.java
+++ src/MinimumAbsoluteDifference.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,47 +13,53 @@ import org.junit.Test;
 
 
 /**
- * @title	1200. Minimum Absolute Difference
+ * @title	383. Ransom Note
  *
  * @desc	
- * 			Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements. 
- * 			Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
- * 			a, b are from arr
- * 			a < b
- * 			b - a equals to the minimum absolute difference of any two elements in arr
+ * 			Given an arbitrary ransom note string and another string containing letters from all the magazines, 
+ * 			write a function that will return true if the ransom note can be constructed from the magazines 
+ * 			therwise, it will return false.
+ * 			Each letter in the magazine string can only be used once in your ransom note.
+ * 
  */
 
 public class MinimumAbsoluteDifference {
 	
-	 public List<List<Integer>> minimumAbsDifference(int[] arr) {
-	    
-	    List<List<Integer>> list=new ArrayList<>();
-	    int min = Integer.MAX_VALUE;
-	    
-	    Arrays.sort(arr);
-	    for (int i = 0; i < arr.length-1; i++) {
-	       if(Math.abs(arr[i]-arr[i+1])==min){
-	           list.add(Arrays.asList(arr[i],arr[i+1]));
-	       } 
-	        else if(Math.abs(arr[i]-arr[i+1])<min){
-	            min=Math.abs(arr[i]-arr[i+1]);
-	            list.clear();
-	            list.add(Arrays.asList(arr[i],arr[i+1]));
-	        }
-	    }
-	    return list;
+	public boolean canConstruct(String ransomNote, String magazine) {
+        
+		char[] ransomArr = ransomNote.toCharArray();
+		char[] magazineArr = magazine.toCharArray();
+		
+		HashMap<Object, Integer> ransomMap = new HashMap<Object, Integer>();
+		HashMap<Object, Integer> magazineMap = new HashMap<Object, Integer>();
+		
+		for (char c : ransomArr) 
+			ransomMap.put(c, ransomMap.getOrDefault(c, 0)+1);
+		
+		for (char c : magazineArr) 
+			magazineMap.put(c, magazineMap.getOrDefault(c, 0)+1);
+		
+		for (char c : ransomArr) {
+			if (!magazineMap.containsKey(c))
+				return false;
+			if (magazineMap.get(c) < ransomMap.get(c))
+				return false;
+		}
+		
+		return true;
     }
 	
 
 	
 	@Test
     public void test_Deck() {
-		int[] deck = {4,2,1,3};
+		
+		String ransomNote = "fffbfg";
+		String magazine = "effjfggbffjdgbjjhhdegh";
 
-		List<List<Integer>> result = minimumAbsDifference(deck); // 함수 실행
-		List<List<Integer>> answer = new ArrayList<>();
+		boolean result = canConstruct(ransomNote, magazine); 
 		List<Integer> temp = new ArrayList<>();
 		
-        assertEquals(answer, result); // 결과 확인
+        assertEquals(true, result); 
     }
 }
