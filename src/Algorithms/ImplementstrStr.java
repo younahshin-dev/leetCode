52ae43c042ad142850dc340275200c5efbf97281
younahshin-dev <50191933+younahshin-dev@users.noreply.github.com>
2020-09-25 09:31:48 +0900

    Create ImplementstrStr.java
     28. Implement strStr() sol

---
+++ src/Algorithms/ImplementstrStr.java
@@ -0,0 +1,10 @@
+/*
+  28. Implement strStr()
+
+  Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
