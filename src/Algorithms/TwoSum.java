dc5f338365615de4e404f154d038a738ecc997cc
younahshin-dev <roaving@gmail.com>
2022-03-04 23:58:47 +0900

    once commit algorithms problem before 24.

---
+++ src/Algorithms/TwoSum.java
@@ -0,0 +1,37 @@
+package Algorithms;
+
+import common.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = null;
+
+        return result;
+    }
+
+    @Test
+    public void test_twosum() {
+
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        assertArrayEquals(new int[]{0, 1}, result);
+    }
+}
