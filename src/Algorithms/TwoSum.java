cbae11cd203a9add6762724e78668550b03720b5
younahshin-dev <roaving@gmail.com>
2022-03-05 00:41:58 +0900

    solve the Two Sum problem

--- src/Algorithms/TwoSum.java
+++ src/Algorithms/TwoSum.java
@@ -20,7 +20,18 @@ import static org.junit.Assert.assertEquals;
  * */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = null;
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+
+                    return result;
+                }
+            }
+        }
 
         return result;
     }
