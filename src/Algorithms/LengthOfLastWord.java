c48dddfa072d11d885849a88d61669cfed12c40b
younahshin-dev <roaving@gmail.com>
2022-03-08 00:01:40 +0900

    make it faster

--- src/Algorithms/LengthOfLastWord.java
+++ src/Algorithms/LengthOfLastWord.java
@@ -7,16 +7,27 @@ import org.junit.Test;
  * Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
  *
  * A word is a maximal substring consisting of non-space characters only.
+ *
+ * Example 2:
+ *
+ * Input: s = "   fly me   to   the moon  "
+ * Output: 4
+ * Explanation: The last word is "moon" with length 4.
+ *
  * */
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
 
-        s = s.replaceAll("\\s+", " ");
-        String temp = "";
+        //s = s.replaceAll("\\s+", " "); //굳이 공백갯수 신경 쓰지 않아도 split 하면 하나로 바뀌어 지느건가?
         String[] arrayWord = s.split(" ");
-        temp = arrayWord[arrayWord.length -1];
+        return arrayWord[arrayWord.length -1].length();
 
-        return temp.length();
+//        s = s.replaceAll("\\s+", " ");
+//        String temp = "";
+//        String[] arrayWord = s.split(" ");
+//        temp = arrayWord[arrayWord.length -1];
+//
+//        return temp.length();
     }
 
     @Test
